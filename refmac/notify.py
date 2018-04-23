e27c7a02bc41a11fdd8bc0aed1bfa74dee32ce29
Pierce Darragh <pierce.darragh@gmail.com>
2018-04-23 13:27:16 -0600

    Improve Notifier to not require multiple instantiations

--- refmac/notify.py
+++ refmac/notify.py
@@ -1,10 +1,9 @@
 from twilio.rest import Client as TwilioClient
 
-class Client:
-    def __init__(self, account_sid: str, auth_token: str, send_from: str, send_to: str):
+class Notifier:
+    def __init__(self, account_sid: str, auth_token: str, send_from: str):
         self._client = TwilioClient(account_sid, auth_token)
         self.send_from = send_from
-        self.send_to = send_to
 
-    def send_message(self, message: str):
-        self._client.messages.create(to=self.send_to, from_=self.send_from, body=message)
+    def send_message(self, message: str, send_to: str):
+        self._client.messages.create(to=send_to, from_=self.send_from, body=message)
